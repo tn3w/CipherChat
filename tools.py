bda5cd14a0429a34ff553a3758186bdc0ea7e934
tn3w <tn3wA8xxfuVMs2@proton.me>
2023-10-29 15:21:21 +0100

    Version 1.13

--- tools.py
+++ tools.py
@@ -26,7 +26,7 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives.asymmetric import rsa, padding as asy_padding
 
-VERSION = 1.12
+VERSION = 1.13
 
 LOGO = '''
  dP""b8 88 88""Yb 88  88 888888 88""Yb  dP""b8 88  88    db    888888 
